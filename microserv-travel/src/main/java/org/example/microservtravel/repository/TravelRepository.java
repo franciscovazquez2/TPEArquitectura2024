c5d7e89fb0a8106ccb8aecb358ffa997e7850a9d
Emanuel Arispe <eaperispeperez@alumnos.exa.unicen.edu.ar>
2024-11-01 18:45:47 -0300

    Se creo estructura de Microserv. Travel y se configuraron los POM.XML para que puedan heredar las dependencias del padre

---
+++ microserv-travel/src/main/java/org/example/microservtravel/repository/TravelRepository.java
@@ -0,0 +1,7 @@
+package org.example.microservtravel.repository;
+
+import org.example.microservtravel.entity.Travel;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface TravelRepository extends MongoRepository<Travel,String> {
+}
