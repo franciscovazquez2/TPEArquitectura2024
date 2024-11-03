b2dc567aae05e6f8a5648058fac05e03a6316c76
Emanuel Arispe <eaperispeperez@alumnos.exa.unicen.edu.ar>
2024-11-03 18:20:58 -0300

    Borrar linea de travel

--- microserv-travel/src/main/java/org/example/microservtravel/controller/TravelController.java
+++ microserv-travel/src/main/java/org/example/microservtravel/controller/TravelController.java
@@ -10,7 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("api/travel/")
+@RequestMapping("/travel")
 public class TravelController {
 
     @Autowired
