d4d93d62e80a85af4d79cc5e3b1b240c2625d172
FranciscoVazquez <franciscovazquez211@gmail.com>
2024-11-09 09:23:00 -0300

    add EjercicioIntegrador

---
+++ EjercicioIntegrador.java
@@ -0,0 +1,13 @@
+/*public class EjercicioIntegrador extends JpaRepository<PersonaEntity,Long> {
+
+@Query(select new PersonEntityDto("p.firstName+p.lastName" ,pbh.phoneNumber) from peronEntity p
+join PhoneBookEntity pbh on p.id = pbh.id order by p.lastName)
+public List<PersonEntityDto>getPersonNumbers();
+
+//por cuestion de informacion sobre el enunciado se devuelve en
+//la misma consulta el dto ya armado
+
+
+
+}
+*/
