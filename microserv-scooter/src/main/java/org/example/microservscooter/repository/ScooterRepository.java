cf2c35484b6a80035dedbfbdf25efb1ae9a04eae
FranciscoVazquez <franciscovazquez211@gmail.com>
2024-11-15 11:33:55 -0300

    ultimos cambios mergeados con ema

--- microserv-scooter/src/main/java/org/example/microservscooter/repository/ScooterRepository.java
+++ microserv-scooter/src/main/java/org/example/microservscooter/repository/ScooterRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ScooterRepository extends JpaRepository<Scooter,Long> {
@@ -27,7 +28,7 @@ public interface ScooterRepository extends JpaRepository<Scooter,Long> {
             "cos(radians(s.longitude) - radians(:longitude)) + sin(radians(:latitude)) * " +
             "sin(radians(s.latitude)))) < :distance")
     List<ScooterDTO> getNearlyScooters(@Param("latitude") double latitude,
-                                     @Param("longitude") double longitude,
-                                     @Param("distance") double distance);
+                                       @Param("longitude") double longitude,
+                                       @Param("distance") double distance);
 
 }
