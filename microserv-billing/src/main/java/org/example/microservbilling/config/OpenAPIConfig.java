994d4843ca4edd396b318ea5b8b71f5ea6ab0b60
Emanuel Arispe <eaperispeperez@alumnos.exa.unicen.edu.ar>
2024-11-20 23:23:01 -0300

    Se agrega configuracion de authenticate en swagger

--- microserv-billing/src/main/java/org/example/microservbilling/config/OpenAPIConfig.java
+++ microserv-billing/src/main/java/org/example/microservbilling/config/OpenAPIConfig.java
@@ -1,17 +1,30 @@
 package org.example.microservbilling.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.http.HttpHeaders;
 
 import java.util.List;
 
 @OpenAPIDefinition
 @Configuration
+@SecurityScheme(
+        name = "Security Token",
+        description = "Access Token For My API",
+        type = SecuritySchemeType.HTTP,
+        paramName = HttpHeaders.AUTHORIZATION,
+        in = SecuritySchemeIn.HEADER,
+        scheme = "bearer",
+        bearerFormat = "JWT"
+)
 public class OpenAPIConfig {
 
 
